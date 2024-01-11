aa9897a0c299abd7b5ff30bf3c0e0058d228e4cc
Murad Azimov <kalmahik@yahoo.com>
2024-01-11 10:02:25 +0300

    [S8T2][MA] Implement table view
    (cherry picked from commit 990ba3a2dae5560349977783ccc1e46f17b09c30)

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Murad Azimov on 11.01.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
---
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Murad Azimov on 06.01.2024.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+    }
+    
+    func configCell(for cell: ImagesListCell) { }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+
+}
+
+extension ImagesListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+            
+            guard let imageListCell = cell as? ImagesListCell else {
+                return UITableViewCell()
+            }
+            
+            configCell(for: imageListCell)
+            return imageListCell
+        }
+    
+}
