b2d9fad88aa4eb9492ad829f119c560c49dd986c
kalmahik <kalmahik@yahoo.com>
2024-01-13 14:17:59 +0300

    [S8T3][MA] Added correct cell and table layout

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -9,4 +9,13 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var like: UIButton!
+    @IBOutlet var picture: UIImageView!
+    @IBOutlet var date: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 0, bottom: 8, right: 0))
+        contentView.layer.cornerRadius = 16        
+    }
 }
--- ImageFeed/ImageList/ImagesListViewController.swift
+++ ImageFeed/ImageList/ImagesListViewController.swift
@@ -9,34 +9,42 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
     
-    @IBOutlet private var tableView: UITableView!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
     
-    func configCell(for cell: ImagesListCell) { }
+    @IBOutlet private var tableView: UITableView!
+    
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        cell.picture.image = UIImage(named: "\(indexPath.row)")
+        cell.date.text = Date().dateString
+        let image = UIImage(named: indexPath.row % 2 == 0 ? "favorite_active" : "favorite_inactive")
+        cell.like.setImage(image, for: .normal)
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
-
+    
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
-            
-            guard let imageListCell = cell as? ImagesListCell else {
-                return UITableViewCell()
-            }
-            
-            configCell(for: imageListCell)
-            return imageListCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentifier, for: indexPath)
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
         }
+        configCell(for: imageListCell, with: indexPath)
+        return imageListCell
+    }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let image = UIImage(named: "\(indexPath.row)") ?? UIImage()
+        let originalRatio =  image.size.height / image.size.width
+        let imageViewWidth = tableView.bounds.width
+        let cellHeight = imageViewWidth * originalRatio + 8
+        return cellHeight
+    }
 }
