57b7c2f78702f1fa07fad18a51254f7f3dcdda50
kalmahik <kalmahik@yahoo.com>
2024-01-15 09:49:24 +0300

    [S8] Fix outlets linking

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -9,13 +9,12 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
-    @IBOutlet var likeButton: UIButton!
     @IBOutlet var pictureImageView: UIImageView!
+    @IBOutlet var likeButton: UIButton!
     @IBOutlet var dateLabel: UILabel!
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        contentView.frame = contentView.frame.inset(by: cellInsets)
         contentView.layer.cornerRadius = 16
         contentView.frame = contentView.frame.inset(by: cellInsets)
     }
