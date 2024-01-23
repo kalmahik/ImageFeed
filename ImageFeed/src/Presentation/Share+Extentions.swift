62120f205e8742c54388972cf64f39804039d301
kalmahik <kalmahik@yahoo.com>
2024-01-23 08:48:49 +0300

    [S9][MA] Share modal

---
+++ ImageFeed/src/Presentation/Share+Extentions.swift
@@ -0,0 +1,8 @@
+//
+//  Share+Extentions.swift
+//  ImageFeed
+//
+//  Created by Admin on 23.01.2024.
+//
+
+import Foundation
--- ImageFeed/src/Presentation/SingleImageViewController.swift
+++ ImageFeed/src/Presentation/SingleImageViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LinkPresentation
 
 class SingleImageViewController: UIViewController {
     var imageName: String!
@@ -16,10 +17,35 @@ class SingleImageViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func shareImageButton(_ sender: UIButton) {
+        let image = UIImage(named: imageName)!
+        let activityViewController = UIActivityViewController(activityItems: [image, self], applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         zoomImageView.imageName = imageName
     }
     
+}
+
+extension SingleImageViewController: UIActivityItemSource {
+    func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+        let image = UIImage(named: imageName)!
+        let imageProvider = NSItemProvider(object: image)
+        let metadata = LPLinkMetadata()
+        metadata.imageProvider = imageProvider
+        return metadata
+    }
+    
+    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
+        return "123"
+    }
+    
+    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
+        return nil
+    }
 }
 
