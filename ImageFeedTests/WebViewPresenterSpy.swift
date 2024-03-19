212452490d8b53dc7264198437c39db2729cc738
kalmahik <kalmahik@yahoo.com>
2024-03-19 10:30:21 +0300

    [S13][MA] Start unit testing

---
+++ ImageFeedTests/WebViewPresenterSpy.swift
@@ -0,0 +1,19 @@
+import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var viewDidLoadCalled: Bool = false
+    var view: WebViewViewControllerProtocol?
+
+    func loadWebview() {
+        viewDidLoadCalled = true
+    }
+
+    func didUpdateProgressValue(_ newValue: Double) {
+
+    }
+
+    func parseCode(from url: URL) -> String? {
+        return nil
+    }
+}
